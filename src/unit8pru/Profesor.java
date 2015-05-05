ebc718984667dd9a70eb70adc8a7023cc0ac2345
Fraini <jarabaco@gmail.com>
2015-05-05 16:18:02 +0200

    Añadido Atributo Salario a la clase profesor, con su get y set

--- src/unit8pru/Profesor.java
+++ src/unit8pru/Profesor.java
@@ -1,19 +1,31 @@
 /**
  * 
  */
-package unit8;
+package unit8pru;
 
 /**
  * @author Fraini Sánchez
  *
  */
 public class Profesor extends Persona {
-
+	
+	private int salario;
+	
 	Profesor(String nombre, String dni) {
 		super(nombre, dni);
 		// TODO Auto-generated constructor stub
 	}
 	
+	
+	public int getSalario() {
+		return salario;
+	}
+
+	public void setSalario(int salario) {
+		this.salario = salario;
+	}
+
+
 	public boolean esProfesor(){
 		return false;
 	}
