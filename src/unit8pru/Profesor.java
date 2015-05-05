9d5525e8b30d99d80bf97775324eda26dd94c4e8
Fraini <jarabaco@gmail.com>
2015-05-05 16:41:17 +0200

    profesor sin salario

--- src/unit8pru/Profesor.java
+++ src/unit8pru/Profesor.java
@@ -8,24 +8,12 @@ package unit8pru;
  *
  */
 public class Profesor extends Persona {
-	
-	private int salario;
-	
+
 	Profesor(String nombre, String dni) {
 		super(nombre, dni);
 		// TODO Auto-generated constructor stub
 	}
 	
-	
-	public int getSalario() {
-		return salario;
-	}
-
-	public void setSalario(int salario) {
-		this.salario = salario;
-	}
-
-
 	public boolean esProfesor(){
 		return false;
 	}
